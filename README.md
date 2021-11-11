c09b4e49a3fc324b9b0c02b14f4f682177488c6b
Molly Wobbles <molly.rendon@outlook.com>
2021-11-10 22:50:20 -0500

    Added questions for quiz, updated README file, and started on styling

---
+++ README.md
@@ -0,0 +1,17 @@
+# Code Quiz
+
+# Purpose
+
+A quiz webstite used to test knowledge of student.
+
+# Built With
+
+HTML
+CSS
+Javascript
+
+# Website
+
+# Contribution
+
+Made by Molly Rendon
---
+++ assets/script.js
@@ -0,0 +1,102 @@
+//Questions and Answers for quiz
+
+//Q One
+//How do you make a comment in HTML?
+//1.  //Comment
+//2.  <!--Comment-->
+//3.  /*Comment*/
+//4.  /Comment
+
+//Answer: 2.  <!--Comment-->
+
+
+//Q Two
+//How do you change the background color to 'dark blue' in CSS?
+//1.  background-color:  dark blue;
+//2.  color-background:  darkblue
+//3.  background-color:  darkblue
+//4.  background-color: darkblue;
+
+//Answer:  4.  background-color: darkblue;
+
+
+//Q Three
+//In which part of the HTML document is the style.css sheet linked?
+//1.  Head
+//2.  Body
+//3.  Header
+//4.  Main
+
+//Answer:  1.  Head
+
+
+//Q Four
+//How do you check the status of your files in Git?
+//1.  git-status
+//2.  check status
+//3.  git status
+//4.  file status
+
+//Answer:  3.  git status
+
+
+
+//Q Five
+//What is the purpose of the alt attribute on an img tag?
+//1.  To add a description to the image to make it more accessable 
+//2.  To change the size of the image
+//3.  To make the image circular 
+//4.  To make the image brighter
+
+//Answer:  1.  
+
+
+//Q Six
+//Which of these are position values?
+//1.  None of the above
+//2.  All of the above
+//3.  Fixed, sticky, and static
+//4.  Relative and absolute
+
+//Answer:  2.  
+
+
+
+//Q Seven
+//What does the box model consist of?
+//1.  Margin and Border
+//2.  Padding and Content
+//3.  None of the above
+//4.  All of the above
+
+//Answer:  4.  
+
+
+//Q Eight
+//What are the four values of the margin property? 
+//1.  Top and bottom
+//2.  Left and right
+//3.  All of the above
+//4.  None of the above
+
+//Answer:  3.  All of the above
+
+
+//Q Nine
+//How do you check the your current branch in Git?  
+//1.  git branch
+//2.  git checkout
+//3.  git status
+//4.  git current
+
+//Answer:  1.  
+
+
+//Q 10
+//How do you make a commit to GitHub with a message?
+//1.  commit -m "commit message"
+//2.  git commit -m "commit message"
+//3.  commit "commit message"
+//4.  git commit push "commit message"
+
+//Answer:  2.  
